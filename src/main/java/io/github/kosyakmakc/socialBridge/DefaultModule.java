7c828daf03f74ae71b9a949213c6de92b0039270
Chovnyuk <kosyakmakc@live.com>
2026-01-27 22:04:34 +0500

    cleanup

--- src/main/java/io/github/kosyakmakc/socialBridge/DefaultModule.java
+++ src/main/java/io/github/kosyakmakc/socialBridge/DefaultModule.java
@@ -1,16 +1,9 @@
 package io.github.kosyakmakc.socialBridge;
 
-import io.github.kosyakmakc.socialBridge.Commands.Arguments.CommandArgument;
-import io.github.kosyakmakc.socialBridge.Commands.MinecraftCommands.MinecraftCommandBase;
-import io.github.kosyakmakc.socialBridge.Commands.MinecraftCommands.MinecraftCommandExecutionContext;
 import io.github.kosyakmakc.socialBridge.DatabasePlatform.DefaultTranslations.English;
 import io.github.kosyakmakc.socialBridge.DatabasePlatform.DefaultTranslations.Russian;
 import io.github.kosyakmakc.socialBridge.MinecraftPlatform.IMinecraftPlatform;
 import io.github.kosyakmakc.socialBridge.Modules.SocialModule;
-import io.github.kosyakmakc.socialBridge.Utils.MessageKey;
-
-import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class DefaultModule extends SocialModule {
@@ -21,18 +14,5 @@ public class DefaultModule extends SocialModule {
         super(loader, loader.getSocialBridgeVersion(), MODULE_ID, MODULE_NAME);
         addTranslationSource(new English());
         addTranslationSource(new Russian());
-
-        addMinecraftCommand(new MinecraftCommandBase(
-            "word",
-            MessageKey.EMPTY,
-            "perm",
-            List.of(CommandArgument.ofWord("food", new String[] { "apple", "banana "}))) {
-
-                @Override
-                public void execute(MinecraftCommandExecutionContext context, List<Object> args) {
-                    context.getSender().sendMessage("aaa", new HashMap<>());
-                }
-                
-            });
     }
 }
